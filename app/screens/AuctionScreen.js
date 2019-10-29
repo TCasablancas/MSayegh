569db1afa0b772ebfcf87913a345171cfd4882b5
Thiago Silva <thyago_casablancas@hotmail.com>
2019-10-29 15:12:04 -0300

    Update AuctionScreen.js

--- app/screens/AuctionScreen.js
+++ app/screens/AuctionScreen.js
@@ -10,6 +10,8 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ActivityIndicator,
+  FlatList
 } from 'react-native';
 
 import styles from '../screens/Styles/HomeStyles';
@@ -18,30 +20,77 @@ import TabStyles from '../screens/Styles/TabStyles';
 import Tabs from '../components/Tabs';
 import ItemAuction from '../components/ItemAuction';
 
-export default function AuctionScreen() {
-  return (
-    <>
-      <Header 
-        centerComponent={ <Image source={ Logo } /> }
-        backgroundColor= "#000"
-      />
-      
-      <ScrollView style={styles.container}>
-        <View>
-          <View><Text style={styles.title}>Nossos últimos leilões</Text></View>
-          <Tabs />
-        </View>
+class AuctionScreen extends React.Component {
 
-        <View>
-          <View><Text style={styles.title}>Leilões ativos</Text></View>
-          <ItemAuction style={ styles.container } />
-        </View>
-      </ScrollView>
-    </>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      dataSource:[]
+    };
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then((responseJson)=> {
+      this.setState({
+        loading: false,
+        dataSource: responseJson
+      })
+    })
+    .catch(error=>console.log(error)) //to catch the errors if any
+  }
+
+  render() {
+
+    if(this.state.loading){
+    return (
+      <View style={styles.loader}> 
+        <ActivityIndicator size="large" color="#000" centerComponent/>
+      </View>
+    )}
+    return (
+      <>
+      
+        <Header 
+          centerComponent={ <Image source={ Logo } /> }
+          backgroundColor= "#000"
+        />
+        
+        <ScrollView style={styles.container}>
+          <View>
+            <View><Text style={styles.title}>Nossos últimos leilões</Text></View>
+            <Tabs />
+          </View>
+
+          <View>
+            <View><Text style={styles.title}>Leilões ativos</Text></View>
+            <ItemAuction style={ styles.container } />
+          </View>
+          
+          <FlatList
+              data= {this.state.dataSource}
+              ItemSeparatorComponent = {this.FlatListItemSeparator}
+              renderItem= {item=> this.renderItem(item)}
+              keyExtractor= {item=>item.id.toString()}
+          />
+
+        </ScrollView>
+      </>
+    );
+  }
+
+  renderItem = (data) =>
+  <TouchableOpacity style={styles.list}>
+  <Text style={styles.lightText}>{data.item.name}</Text>
+  <Text style={styles.lightText}>{data.item.email}</Text>
+  <Text style={styles.lightText}>{data.item.company.name}</Text></TouchableOpacity>
 }
 
 AuctionScreen.navigationOptions = {
   //title: "Leilões",
   header: null
 };
+
+export default AuctionScreen;
